0dd9b70d57dc181c334e0c8884ed2ac270eb4ce5
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-08-12 13:31:37 +0200

    Implement integrator component with an initial flow to move messages from MQTT to Kafka

---
+++ integrator/src/main/java/com/dreamsoftware/integrator/config/IntegrationConfig.java
@@ -0,0 +1,102 @@
+package com.dreamsoftware.integrator.config;
+
+import jakarta.annotation.PostConstruct;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.core.GenericHandler;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.endpoint.MessageProducerSupport;
+import org.springframework.integration.kafka.dsl.Kafka;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableIntegration
+@IntegrationComponentScan
+public class IntegrationConfig {
+
+    @Value("${MQTT_BROKER}")
+    private String mqttBroker;
+
+    @Value("${MQTT_TOPIC}")
+    private String mqttTopic;
+
+    @Value("${KAFKA_BROKER}")
+    private String kafkaBroker;
+
+    @Value("${KAFKA_TOPIC}")
+    private String kafkaTopic;
+
+
+    @PostConstruct
+    public void logEnvironmentVariables() {
+        System.out.println("MQTT Broker: " + mqttBroker);
+        System.out.println("MQTT Topic: " + mqttTopic);
+        System.out.println("Kafka Broker: " + kafkaBroker);
+        System.out.println("Kafka Topic: " + kafkaTopic);
+    }
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel kafkaOutboundChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageProducerSupport mqttInbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter(mqttBroker, "clientId", mqttTopic);
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        return adapter;
+    }
+
+    @Bean
+    public GenericHandler<Message<?>> messageHandler() {
+        return (message, headers) -> {
+            // Process the MQTT message here
+            System.out.println("messageHandler MQTT message: " + message);
+            return message;
+        };
+    }
+
+    @Bean
+    public IntegrationFlow mqttToKafkaFlow() {
+        return IntegrationFlow.from(mqttInbound())
+                .channel(mqttInputChannel())
+                .handle(messageHandler())
+                .channel(kafkaOutboundChannel())
+                .handle(Kafka.outboundChannelAdapter(kafkaProducerFactory())
+                        .messageKey(m -> m.getHeaders().get("mqtt_topic"))
+                        .topicExpression("headers['kafka_topic']"))
+                .get();
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> kafkaProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBroker);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+}
