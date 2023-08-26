c86590e7588326af1fd27b230c500d1441a5da49
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-08-26 09:25:30 +0200

    Get  code hash of fog node from vault and use it in order to generate CHAP challenge result. Generate a session identifier  which will be storage in redis during 1 hour and return as part of client autenticate response

--- authentication/app.py
+++ authentication/app.py
@@ -27,6 +27,18 @@ def get_stored_password(mac_address):
     except Exception as e:
         raise Exception("Error retrieving stored password from Vault")
 
+def get_code_hash(mac_address):
+    try:
+        response = requests.get(
+            f"{VAULT_ADDRESS}/v1/secret/data/fog_nodes/{mac_address}",
+            headers={"X-Vault-Token": VAULT_TOKEN}
+        )
+        response_json = response.json()
+        code_hash = response_json["data"]["code_hash"]
+        return code_hash
+    except Exception as e:
+        raise Exception("Error retrieving code hash from Vault")
+    
 app = Flask(__name__)
 
 # Redis Configuration
@@ -44,13 +56,18 @@ def get_challenge():
         mac_address = data.get('mac_address')
         
         stored_password = get_stored_password(mac_address)
-        
-        challenge = str(uuid.uuid4())  # Generar un reto único
+        code_hash = get_code_hash(mac_address)
 
-        # Store the password + challenge in Redis
+        challenge = str(uuid.uuid4())  # Generate challenge
+
+        # Store the CHAP challenge result in Redis with a 2-minute expiration
         redis_key = f"{mac_address}_challenge"
-        redis_client.set(redis_key, hashlib.sha256((stored_password + challenge).encode()).hexdigest())
+        redis_client.set(redis_key, hashlib.sha256((stored_password + challenge + code_hash).encode()).hexdigest(), ex=120)
         
+        # Log the challenge and generated UUID
+        print(f"Challenge generated for MAC {mac_address}: {challenge}")
+        print(f"Generated UUID: {challenge_data['uuid']}")
+
         return jsonify(challenge=challenge), 200
     except Exception as e:
         return jsonify(message=str(e)), 500
@@ -65,17 +82,26 @@ def authenticate():
         # Retrieve the password + challenge from Redis
         redis_key = f"{mac_address}_challenge"
         stored_result = redis_client.get(redis_key)
-        
+
+        # Delete the challenge key from Redis regardless of authentication outcome
+        redis_client.delete(redis_key)
         if stored_result:
             expected_response = stored_result.decode('utf-8')
             if client_response == expected_response:
-                return jsonify(message='Authentication successful'), 200
+                # Generate a unique UUID for the session
+                session_uuid = str(uuid.uuid4())
+                # Store authenticated session UUID in Redis with a 1-hour expiration
+                redis_session_key = f"{mac_address}_session"
+                redis_client.set(redis_session_key, session_uuid, ex=3600)
+                return jsonify(message='Authentication successful', session_id=session_uuid), 200
             else:
                 return jsonify(message='Authentication failed'), 401
         else:
             return jsonify(message='Challenge not found'), 404
     except Exception as e:
         return jsonify(message=str(e)), 500
+    
+
 
 
 if __name__ == '__main__':
