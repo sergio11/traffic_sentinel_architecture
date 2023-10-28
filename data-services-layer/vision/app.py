0c8da0fdaece888f987b3ec4fe2aadaabaef943a
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-10-28 14:09:52 +0200

    Implement new endpoint to save processed frames

--- data-services-layer/vision/app.py
+++ data-services-layer/vision/app.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 import os
 from datetime import datetime
 from common.requires_authentication_decorator import requires_authentication
+from minio import Minio
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
@@ -13,6 +14,14 @@ MONGO_CONNECTION_URL = os.environ.get("MONGO_CONNECTION_URL", "mongodb://localho
 MONGO_DB = os.environ.get("MONGO_DB", "db")
 mongo_client = MongoClient(MONGO_CONNECTION_URL)
 db = mongo_client[MONGO_DB]
+
+MINIO_ENDPOINT = os.environ.get("MINIO_ENDPOINT", "minio-server:9000")
+MINIO_ACCESS_KEY = os.environ.get("MINIO_ACCESS_KEY", "your-access-key")
+MINIO_SECRET_KEY = os.environ.get("MINIO_SECRET_KEY", "your-secret-key")
+MINIO_BUCKET = "frames-bucket"
+
+minio_client = Minio(MINIO_ENDPOINT, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET_KEY, secure=False)
+
 
 DEFAULT_PAGE_SIZE = 10
 
@@ -111,6 +120,58 @@ def get_frames():
     frames = list(db.frames.find(query, {"_id": 0}).skip((page - 1) * page_size).limit(page_size))
 
     return jsonify(frames=frames)
+
+
+@app.route('/process_frame', methods=['POST'])
+def process_frame():
+    try:
+        data = request.json
+        mac_address = data.get("mac_address")
+        timestamp = data.get("timestamp")
+        frame_data = data.get("frame_data")
+
+        # Check if the required data is provided
+        if not (mac_address and timestamp and frame_data):
+            return jsonify(message="Missing required data"), 400
+
+        # Convert the timestamp to a datetime object
+        timestamp = datetime.fromtimestamp(timestamp)
+
+        # Save the images to MinIO and get the URLs
+        frame_data["annotated_frame_url"] = save_image_to_minio(frame_data["annotated_frame_base64"])
+        frame_data["original_frame_url"] = save_image_to_minio(frame_data["original_frame_base64"])
+        for vehicle in frame_data["detected_vehicles"]:
+            vehicle["vehicle_frame_url"] = save_image_to_minio(vehicle["vehicle_frame_base64"])
+
+        # Remove Base64 representations of the images
+        frame_data.pop("annotated_frame_base64", None)
+        frame_data.pop("original_frame_base64", None)
+        for vehicle in frame_data["detected_vehicles"]:
+            vehicle.pop("vehicle_frame_base64", None)
+
+        # Insert the JSON into the MongoDB collection
+        db.frames.insert_one({
+            "mac_address": mac_address,
+            "timestamp": timestamp,
+            "frame_data": frame_data
+        })
+
+        return jsonify(message="Frame processed successfully"), 200
+
+    except Exception as e:
+        return jsonify(message="Error processing frame", error=str(e)), 500
+
+
+def save_image_to_minio(base64_data):
+    # Generate a unique object name in MinIO
+    object_name = f"{datetime.now().isoformat()}.jpg"
+
+    # Decode the Base64 data and store the image in MinIO
+    minio_client.put_object(MINIO_BUCKET, object_name, base64_data.encode(), len(base64_data))
+
+    # Return the URL of the image in MinIO
+    presigned_url = minio_client.presigned_get_object(MINIO_BUCKET, object_name)
+    return presigned_url
 
 
 if __name__ == "__main__":
