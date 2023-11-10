dc1c10b8a39517181956ea9a1773455962bc8053
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-11-10 15:12:22 +0100

    Implement several improvements in Data Service layer

--- data-services-layer/provision/app.py
+++ data-services-layer/provision/app.py
@@ -11,11 +11,11 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+BASE_URL_PREFIX = "/provision"
 # Redis Configuration
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
 redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
-
 VAULT_ADDRESS = os.environ.get("VAULT_ADDRESS", "http://vault:8200")
 # MongoDB Configuration
 MONGO_CONNECTION_URL = os.environ.get("MONGO_CONNECTION_URL", "mongodb://localhost:27017/")
@@ -23,49 +23,11 @@ MONGO_DB = os.environ.get("MONGO_DB", "db")
 mongo_client = MongoClient(MONGO_CONNECTION_URL)
 db = mongo_client[MONGO_DB]
 
-# Retrieve the stored password for a device based on its MAC address
-@app.route("/get-fog-password", methods=["GET"])
-def get_fog_password():
-    logger.info("Received GET request for get_fog_password")
-
-    # Get MAC address from the request's query parameters
-    mac_address = request.args.get("mac_address")
-
-    if mac_address:
-        logger.debug(f"MAC address provided: {mac_address}")
-        # Call the _get_node_password function to retrieve the node password
-        node_password = _get_stored_password(mac_address)
-
-        if node_password:
-            logger.info(f"Node password retrieved for MAC {mac_address}")
-            # Return a JSON response with a success status
-            response = {
-                "status": "success",
-                "message": "Node password retrieved successfully",
-                "fog_password": node_password
-            }
-            return jsonify(response), 200
-        else:
-            logger.warning("Node password not found")
-            # Return a JSON response with an error status and message
-            response = {
-                "status": "error",
-                "message": "Node password not found"
-            }
-            return jsonify(response), 404
-    else:
-        logger.error("MAC address not provided in request")
-        # Return a JSON response with a bad request status and message
-        response = {
-            "status": "error",
-            "message": "MAC address not provided in the request"
-        }
-        return jsonify(response), 400
 
 # Provision camera information based on the MAC address
-@app.route("/provision", methods=["GET"])
-def provision_camera():
-    logger.info("Received GET request for provision_camera")
+@app.route(f"{BASE_URL_PREFIX}/node", methods=["GET"])
+def get_node_provision():
+    logger.info("Received GET request for get_node_provision")
     
     # Get MAC address from the request's query parameters
     mac_address = request.args.get("mac_address")
@@ -96,7 +58,7 @@ def provision_camera():
                     redis_client.delete(redis_session_key)
                     response = {
                         "status": "success",
-                        "message": "Camera provisioned successfully",
+                        "message": "Node provisioned successfully",
                         "camera_url": camera_url,
                         "camera_url_params": camera_url_params,
                         "camera_username": camera_username,
@@ -133,9 +95,9 @@ def provision_camera():
         return jsonify(response), 400
 
 # Provision a new camera association
-@app.route("/provision/link", methods=["POST"])
-def provision_camera_link():
-    logger.info("Received POST request for provision_camera_link")
+@app.route(f"{BASE_URL_PREFIX}/node", methods=["POST"])
+def provision_node():
+    logger.info("Received POST request for provision_node")
     
     # Get JSON data from the request's body
     data = request.get_json()
@@ -174,17 +136,17 @@ def provision_camera_link():
         }
     )
 
-    logger.info(f"Camera provisioned successfully for MAC {mac_address}")
+    logger.info(f"Node provisioned successfully for MAC {mac_address}")
     response = {
         "status": "success",
-        "message": "Camera provisioned successfully"
+        "message": "Node provisioned successfully"
     }
     return jsonify(response), 201
 
 # Remove a camera association based on the MAC address
-@app.route("/provision/remove", methods=["DELETE"])
-def remove_camera_association():
-    logger.info("Received DELETE request for remove_camera_association")
+@app.route(f"{BASE_URL_PREFIX}/node", methods=["DELETE"])
+def remove_node_provision():
+    logger.info("Received DELETE request for remove_node_provision")
 
     # Get MAC address from the request's query parameters
     mac_address = request.args.get("mac_address")
@@ -201,7 +163,7 @@ def remove_camera_association():
             # Delete the camera association from the MongoDB database
             db.provisioning.delete_one({"mac_address": mac_address})
             logger.info(f"Camera association removed for MAC {mac_address}")
-            response["message"] = "Camera association removed"
+            response["message"] = "Node provision removed"
             return jsonify(response), 200
         else:
             logger.warning(f"MAC address {mac_address} not found in database")
@@ -216,12 +178,12 @@ def remove_camera_association():
 
 
 # Get a list of all camera associations
-@app.route("/provision/list", methods=["GET"])
-def get_camera_associations():
-    logger.info("Received GET request for get_camera_associations")
+@app.route(f"{BASE_URL_PREFIX}/list", methods=["GET"])
+def get_node_provision_list():
+    logger.info("Received GET request for get_node_provision_list")
 
     # Initialize the response dictionary
-    response = {"status": "success", "message": "Camera associations retrieved successfully", "data": []}
+    response = {"status": "success", "message": "Node provisions retrieved successfully", "data": []}
 
     # Retrieve all camera associations from the MongoDB database
     cursor = db.provisioning.find({})
@@ -273,59 +235,7 @@ def update_gps_info():
         }
         return jsonify(response), 404
 
-# Helper function to retrieve the Vault token from Redis
-def _get_vault_token():
-    """
-    Helper function to retrieve the Vault token from Redis.
-
-    This function retrieves the Vault root token from Redis, which is used for authentication
-    when making requests to Vault for retrieving secrets.
-
-    Returns:
-        str: The Vault root token.
-        
-    Raises:
-        Exception: If the Vault token is not found in Redis or an error occurs during retrieval.
-    """
-    try:
-        token = redis_client.get("vault_root_token")
-        if token:
-            return token.decode("utf-8")
-        else:
-            raise Exception("Vault token not found in Redis")
-    except Exception as e:
-        raise Exception("Error retrieving Vault token from Redis", e)
-    
-
-def _get_stored_password(mac_address):
-    """
-    Helper function to retrieve the stored password for a MAC address from Vault.
-
-    This function makes a request to Vault to retrieve the stored password for a MAC address.
-    It requires the Vault root token for authentication.
-
-    Args:
-        mac_address (str): The MAC address of the device.
-        vault_token (str): The Vault root token for authentication.
-
-    Returns:
-        str: The stored password for the MAC address.
-
-    Raises:
-        Exception: If an error occurs during retrieval.
-    """
-    try:
-        # Remove colons from MAC address
-        mac_address = mac_address.replace(":", "")
-        client = hvac.Client(url=VAULT_ADDRESS, token=_get_vault_token())
-        secret = client.secrets.kv.read_secret(
-            path="fog-nodes-v1/" + mac_address
-        )
-        stored_password = secret["data"]["data"]
-        return stored_password
-    except Exception as e:
-        raise Exception("Error retrieving stored password from Vault", e)
 
 if __name__ == "__main__":
     # Start the Flask application
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
