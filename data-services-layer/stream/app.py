f27546f9eaf282d3b13821c963d8c551881f57f0
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-11-18 21:56:01 +0100

    Implement several improvements in Streaming API Sservice

--- data-services-layer/stream/app.py
+++ data-services-layer/stream/app.py
@@ -6,27 +6,44 @@ import logging
 from pymongo import MongoClient
 from bson import ObjectId
 
+# Event logging configuration
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 # MongoDB Configuration
 MONGO_CONNECTION_URL = os.environ.get("MONGO_CONNECTION_URL", "mongodb://localhost:27017/")
-MONGO_DB = os.environ.get("MONGO_DB", "db")
+MONGO_DB = os.environ.get("MONGO_DB", "smarthighwaynet")
 mongo_client = MongoClient(MONGO_CONNECTION_URL)
 db = mongo_client[MONGO_DB]
 
+# Initialize the client manager
+clientManager = ClientManager(logger)
+
+# Initialize Flask app and SocketIO
 app = Flask(__name__)
 socketio = SocketIO(app)
+socketio.on_namespace(clientManager)
 
-# Initialize the namespace for managing client connections
-clientManager = ClientManager()
+# Socket event handlers
+
+@socketio.on('connect')
+def on_connect():
+    # Method called when a client connects to the server
+    client_sid = request.sid
+    clientManager.on_connect(client_sid)
+     
+@socketio.on('disconnect')
+def on_disconnect():
+    # Method called when a client disconnects from the server
+    client_sid = request.sid
+    clientManager.on_disconnect(client_sid)
 
 @socketio.on('subscribe_camera')
 def subscribe_camera(data):
-    global clientManager
+    # Method called when a client requests to subscribe to a camera
     client_sid = request.sid
     camera_id = data.get('camera_id')
-    logger.info(f"Client {client_sid} try to subscribe to camera {camera_id}")
+    logger.info(f"Client {client_sid} tries to subscribe to camera {camera_id}")
     camera = db.cameras.find_one({'_id': ObjectId(camera_id)})
     if camera is not None:
         clientManager.subscribe_to_camera(client_sid, camera_id)
@@ -36,6 +53,16 @@ def subscribe_camera(data):
         emit('subscription_error', {'message': "Invalid camera ID"}, status=400)
         logger.error(f"Invalid camera ID subscription request from client {client_sid}")
 
+@socketio.on('unsubscribe_camera')
+def unsubscribe_camera(data):
+    # Method called when a client requests to unsubscribe from a camera
+    client_sid = request.sid
+    camera_id = data.get('camera_id')
+    logger.info(f"Client {client_sid} tries to unsubscribe from camera {camera_id}")
+    clientManager.unsubscribe_from_camera(client_sid)
+    emit('unsubscription_success', {'message': f"Unsubscribed from camera ID {camera_id}"})
+    logger.info(f"Client {client_sid} unsubscribed from camera {camera_id}")
+
 if __name__ == '__main__':
-    socketio.on_namespace(clientManager)
+    # Server execution
     socketio.run(app, debug=True, use_reloader=False)
--- data-services-layer/stream/client_manager.py
+++ data-services-layer/stream/client_manager.py
@@ -1,12 +1,9 @@
-import logging
+import json
 import os
 import threading
-from flask import request
 from confluent_kafka import Consumer, KafkaError
 from flask_socketio import Namespace, join_room, leave_room
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
 
 # Load configuration from environment variables
 KAFKA_BOOTSTRAP_SERVERS = os.environ.get('KAFKA_BROKER', 'localhost:9092')
@@ -20,32 +17,33 @@ class ClientManager(Namespace):
     Attributes:
     - clients (dict): A dictionary to store connected clients and their subscribed camera IDs.
     """
-    def __init__(self):
+    def __init__(self, logger):
         """
         Initializes the ClientManager.
         """
         super().__init__(namespace='/traffic_sentinel_stream')
         self.clients = {}
         self.kafka_thread = None
+        self.logger = logger
 
-    def on_connect(self):
+    def on_connect(self, client_sid):
         """
         Handles a new client connection and adds it to the clients dictionary.
         """
-        client_sid = request.sid
         self.clients[client_sid] = None  # Initially, the client is not subscribed to any camera
-        logger.info(f"Client {client_sid} connected")
+        self.logger.info(f"Client {client_sid} connected")
 
-    def on_disconnect(self):
+    
+    def on_disconnect(self, client_sid):
         """
         Handles a client disconnection and removes it from the clients dictionary.
         """
-        client_sid = request.sid
         if client_sid in self.clients:
             # Unsubscribe from camera before disconnecting
             self.unsubscribe_from_camera(client_sid)
             del self.clients[client_sid]
-            logger.info(f"Client {client_sid} disconnected")
+            self.logger.info(f"Client {client_sid} disconnected")
+            
 
     def subscribe_to_camera(self, client_sid, camera_id):
         """
@@ -56,14 +54,14 @@ class ClientManager(Namespace):
         - camera_id: ID of the camera to subscribe to.
         """
         if client_sid in self.clients:
-            if self.clients[client_sid] != camera_id:
+            if self.clients[client_sid] is not None and self.clients[client_sid] != camera_id:
                 # Unsubscribe from previous camera room if already subscribed
                 self.unsubscribe_from_camera(client_sid)
 
             self.clients[client_sid] = camera_id
             join_room(camera_id)
-            logger.info(f"Client {client_sid} subscribed to camera {camera_id}")
-            logger.info(f"Check Clients and start kafka consumer")
+            self.logger.info(f"Client {client_sid} subscribed to camera {camera_id}")
+            self.logger.info(f"Check Clients and start kafka consumer")
             self._check_clients_and_start_kafka_consumer()
 
     def unsubscribe_from_camera(self, client_sid):
@@ -77,23 +75,22 @@ class ClientManager(Namespace):
             camera_id = self.clients[client_sid]
             leave_room(camera_id)
             self.clients[client_sid] = None
-            logger.info(f"Client {client_sid} unsubscribed from camera {camera_id}")
+            self.logger.info(f"Client {client_sid} unsubscribed from camera {camera_id}")
         else:
-            logger.info(f"Client {client_sid} is not subscribed to any camera")
+            self.logger.info(f"Client {client_sid} is not subscribed to any camera")
 
     def _check_clients_and_start_kafka_consumer(self):
-        logger.info("_check_clients_and_start_kafka_consumer ...")
         if len(self.clients) > 0 and self.kafka_thread is None:
             self.kafka_thread = threading.Thread(target=self._start_kafka_consumer)
             self.kafka_thread.start()
-            logger.info("Started Kafka consumer thread because there are connected clients")
+            self.logger.info("Started Kafka consumer thread because there are connected clients")
         elif len(self.clients) == 0 and self.kafka_thread is not None:
             self.kafka_thread = None
-            logger.info("Stopped Kafka consumer thread because there are no connected clients")
+            self.logger.info("Stopped Kafka consumer thread because there are no connected clients")
         elif len(self.clients) > 0 and self.kafka_thread is not None:
-            logger.info("Kafka consumer thread is already running and there are connected clients")
+            self.logger.info("Kafka consumer thread is already running and there are connected clients")
         else:
-            logger.info("No action taken as there are no connected clients and Kafka consumer thread is not running")
+            self.logger.info("No action taken as there are no connected clients and Kafka consumer thread is not running")
 
     def _start_kafka_consumer(self):
         """
@@ -106,7 +103,7 @@ class ClientManager(Namespace):
         })
         c.subscribe([KAFKA_TOPIC])
 
-        logger.info("Kafka consumer started")
+        self.logger.info("Kafka consumer started")
 
         while True:
             msg = c.poll(1.0)
@@ -116,15 +113,18 @@ class ClientManager(Namespace):
                 if msg.error().code() == KafkaError._PARTITION_EOF:
                     continue
                 else:
-                    logger.info(f"Kafka Error: {msg.error().str()}")
+                    self.logger.info(f"Kafka Error: {msg.error().str()}")
                     break
             else:
                 frame_data = msg.value().decode('utf-8')
-                logger.info(f"Received message from Kafka: {frame_data}")
-                for client_sid, camera_id in self.clients.items():
-                    if camera_id and camera_id == msg.camera_id:
+                payload = json.loads(frame_data) 
+                camera_id = payload.get('camera_id')
+                self.logger.info(f"Received new message from Kafka with camera id: {camera_id}")
+
+                for client_sid, subscribed_camera_id in self.clients.items():
+                    if subscribed_camera_id and subscribed_camera_id == camera_id:
                         self.emit('new_frame', frame_data, namespace='/traffic_sentinel_stream', room=client_sid)
-                        logger.info(f"Emitted new_frame event to client {client_sid} for camera {camera_id}")  # Log de emisión del mensaje a clientes
+                        self.logger.info(f"Emitted new_frame event to client {client_sid} for camera {camera_id}")
 
         c.close()
-        logger.info("Kafka consumer stopped")
+        self.logger.info("Kafka consumer stopped")
