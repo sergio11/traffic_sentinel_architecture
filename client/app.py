46e69eac17e456ef9c51f701a594c0b74c3e15ab
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-11-18 21:55:14 +0100

    Implement several improvements in Client Desktop GUI

--- client/app.py
+++ client/app.py
@@ -1,8 +1,19 @@
 from io import BytesIO
+import logging
 import os
+import threading
 import tkinter as tk
 from PIL import Image, ImageTk
 import base64
+import requests
+import socketio
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+AUTHENTICATE_ENDPOINT = os.environ.get("AUTHENTICATE_ENDPOINT", "http://localhost:5003/users/authenticate")
+STREAM_SERVICE_ENDPOINT = os.environ.get("STREAM_SERVICE_ENDPOINT", "http://localhost:5004")
+STREAM_SERVICE_NAMESPACE = os.environ.get("STREAM_SERVICE_NAMESPACE", "/traffic_sentinel_stream")
 
 def display_login_screen():
     root = tk.Tk()
@@ -35,13 +46,22 @@ def display_login_screen():
         image_label.pack(pady=(0, 10))  # Add vertical space above and below
 
     def login():
-        # Check credentials (dummy example)
-        if entry_username.get() == "admin" and entry_password.get() == "password":
-            # Destroy login window and open main payload window
-            root.destroy()
-            display_home_screen()
-        else:
-            label_error.config(text="Invalid credentials", fg="red")
+        username = entry_username.get()
+        password = entry_password.get()
+        payload = {"username": username, "password": password}
+
+        def authenticate_user():
+            response = requests.post(AUTHENTICATE_ENDPOINT, json=payload)
+            if response.status_code == 200:
+                logger.info("User authenticated successfully.")
+                root.event_generate("<<SuccessfulAuth>>", when="tail")
+            else:
+                logger.error("Invalid credentials provided.")
+                root.event_generate("<<UnSuccessfulAuth>>", when="tail")
+            logger.info("Authentication initiated.")
+
+        auth_thread = threading.Thread(target=authenticate_user)
+        auth_thread.start()
 
     label_username = tk.Label(content_frame, text="Username", font=("Arial", 12, "bold"))
     label_username.pack(pady=(20, 0))
@@ -56,6 +76,16 @@ def display_login_screen():
     label_error = tk.Label(content_frame, text="", fg="red", font=("Arial", 10))
     label_error.pack(pady=(20, 0))
 
+    def on_successful_auth(event):
+        root.destroy()
+        display_home_screen()
+
+    def on_unsuccessful_auth(event):
+        label_error.config(text="Invalid credentials", fg="red")
+
+    root.bind("<<SuccessfulAuth>>", on_successful_auth)
+    root.bind("<<UnSuccessfulAuth>>", on_unsuccessful_auth)
+
     root.mainloop()
 
 
@@ -63,10 +93,10 @@ def display_home_screen():
     root = tk.Tk()
     root.title("Traffic Sentinel - HOME")
     root.geometry("800x600")
+
     project_title = tk.Label(root, text="Traffic Sentinel - Driving Smarter Roads with IoT Traffic Monitoring",
                              font=("Arial", 16, "bold"))
     project_title.pack(pady=10)
-
     info_frame = tk.Frame(root)
     info_frame.pack(padx=10, pady=10)
 
@@ -77,28 +107,44 @@ def display_home_screen():
         value_label = tk.Label(info_frame, text=value, font=("Arial", 12))
         value_label.pack(anchor=tk.E)
 
-    # Display payload information in a column on the right side
-    create_info_label("MAC Address:", example_payload_with_image['mac_address'])
-    create_info_label("Camera ID:", example_payload_with_image['camera_id'])
-    create_info_label("Frame Timestamp:", example_payload_with_image['frame_timestamp'])
+    def connect_to_server():
+        sio = socketio.Client()
 
-    # Display the base64 image if exists in the payload
-    if 'annotated_frame_base64' in example_payload_with_image:
-        image_base64 = example_payload_with_image['annotated_frame_base64']
-        image = base64.b64decode(image_base64)
-        img = Image.open(BytesIO(image))
-        img.thumbnail((600, 400)) 
-        photo = ImageTk.PhotoImage(img)
-        label_image = tk.Label(root, image=photo)
-        label_image.image = photo
-        label_image.pack()
+        @sio.on('new_frame')
+        def handle_new_frame(payload):
+            logger.info(f"on new frame received")
+            root.event_generate("<<FramePayload>>", when="tail", detail=payload)
+
+        @sio.on('connect')
+        def on_connect():
+            logger.info("Connected to server")
+            camera_id = '654f6baff3d3ad4f84c41f9e'
+            sio.emit('subscribe_camera', {'camera_id': camera_id})
+            logger.info(f"Subscribed to camera {camera_id}")
+
+        sio.connect(STREAM_SERVICE_ENDPOINT, namespaces=[STREAM_SERVICE_NAMESPACE])
+        sio.wait()
+
+    connect_thread = threading.Thread(target=connect_to_server)
+    connect_thread.start()
+
+    def handle_frame_payload(data):
+        create_info_label("MAC Address:", data['mac_address'])
+        create_info_label("Camera ID:", data['camera_id'])
+        create_info_label("Frame Timestamp:", data['frame_timestamp'])
+
+        if 'annotated_frame_base64' in data:
+            image_base64 = data['annotated_frame_base64']
+            image = base64.b64decode(image_base64)
+            img = Image.open(BytesIO(image))
+            img.thumbnail((600, 400))
+            photo = ImageTk.PhotoImage(img)
+            label_image = tk.Label(root, image=photo)
+            label_image.image = photo
+            label_image.pack()
+
+    root.bind("<<FramePayload>>", handle_frame_payload)
 
     root.mainloop()
-
-example_payload_with_image = {
-    "mac_address": "02:42:ac:11:00:02",
-    "camera_id": "654f6baff3d3ad4f84c41f9e",
-    "frame_timestamp": 1699994767
-}
 
 display_login_screen()
